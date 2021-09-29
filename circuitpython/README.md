aca6ce40f94707ef0d6c5ab5408b2183d4f99448
Paul Wallich <paul.wallich@gmail.com>
2021-09-29 11:42:23 -0400

    adding circuitpython stuff and 3d designs

---
+++ circuitpython/README.md
@@ -0,0 +1,2 @@
+The same basic idea only with circuitpython code and generalized to more
+possible vehicles.
---
+++ circuitpython/hackebot/hackebot_helper.py
@@ -0,0 +1,16 @@
+#hackebot helper
+
+
+import time
+import board
+import pulseio
+import digitalio
+import adafruit_dotstar
+
+import adafruit_hcsr04
+from adafruit_motor import servo
+#pwm = pulseio.PWMOut(board.D7, duty_cycle=0, frequency=50)
+#steering = servo.Servo(pwm)
+
+#led = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
+#sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.A3, echo_pin=board.A5)
